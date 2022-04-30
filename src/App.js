bdf0b3f0836c4a6d58edde7b2d97acf8ef309b78
carlesfores <fores.carles@gmail.com>
2022-04-30 22:44:20 +0200

    Add questions config file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import React, { useState } from 'react';
+import { questions } from './data/questions';
 import './App.css';
 
 function App() {
+
+  const [ step, setStep ] = useState(1);
+  const [ progress, setProgress ] = useState([]);
+
   return (
     <div>
 
