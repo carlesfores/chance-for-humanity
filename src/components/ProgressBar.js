5c3cf6cf0cb3af3c19cce430b07d3f84dd8f7b90
carlesfores <fores.carles@gmail.com>
2022-05-01 13:08:36 +0200

    Remove console log

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 export const ProgressBar = React.memo(( { progressLength, questionsLength } ) => {
-    console.log('progress bar', progressLength, questionsLength)
+
     return (
         <div 
             className='progress' 
-            style={{ width: `${( progressLength / questionsLength ) * 100}vw` }}>
+            style={{width: `${(progressLength/questionsLength) * 100}vw`}}>
         </div>  
     );
 });
