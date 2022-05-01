1119b9e90a69056fb7a129283486b4bbe341c718
carlesfores <fores.carles@gmail.com>
2022-05-01 12:33:32 +0200

    Create ProgressBar component

---
+++ src/components/ProgressBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const ProgressBar = React.memo(( {progressLength, questionsLength} ) => {
+    console.log('progress bar', progressLength, questionsLength)
+    return (
+        <div 
+            className='progress' 
+            style={{ width: `${( progressLength / questionsLength ) * 100}vw` }}>
+
+        </div>  
+
+    );
+});
